541a268acf5bf0f2fa6a3e5c1b41ea479ab13df7
Ladislav Vasina <ladislavvasina@gmail.com>
2022-11-29 11:27:55 +0100

    Argument parser

--- main.cpp
+++ main.cpp
@@ -8,15 +8,129 @@
  */
 
 #include "main.h"
+
 using namespace std;
 
-int main(int argc, char *argv[]){
-    Print(" model2 - SIMLIB/C++ example\n");
-    SetOutput("model2.out");
-    Init(0,1000);              // experiment initialization for time 0..1000
-    (new Generator)->Activate(); // customer generator
-    Run();                     // simulation
-    Box.Output();              // print of results
-    Table.Output();
-    return 0;
+argValues args;
+
+// Function for easier program exiting
+void exit_ims(string errorMessage) {
+    fprintf(stderr,
+            "%s%s \nALL ARGUMENTS ACCEPT INTEGERS ONLY%s%s\nUSAGE: ./main\n"
+            " -R How many Qassams per day\n"
+            " -Z How long it takes to fire all rockets per day [minutes] (max value is 1440)\n"
+            " -J Percentage of human error in BMC\n"
+            " -K Percentage of Qassam failure\n"
+            " -M Percentage of Qassams that need operator intervention\n"
+            " -N Uniform time of Qassam flight [seconds]%s\n", RED_BEGIN, errorMessage.c_str(), COLOR_END, CYAN_BEGIN,
+            COLOR_END);
+    exit(EXIT_FAILURE);
 }
+
+string optionToFind;
+
+int main(int argc, char *argv[]) {
+    // Argument parser
+    if (argc >= 2) {
+        for (int i = 1; i < argc; i++) {
+            optionToFind = string(argv[i]);
+            // Help handler
+            if (optionToFind == "-h" || optionToFind == "--help") {
+                exit_ims("");
+            }
+            // Check -R option
+            else if (optionToFind == "-R") {
+                if (argv[i + 1] == nullptr) {
+                    exit_ims("!!!You are missing argument for -R parameter!!!");
+                }
+                string tmp = argv[i + 1];
+                if (tmp[0] == '-') {
+                    exit_ims("!!!-R parameter can't be negative!!!");
+                }
+                args.R = atoi(argv[i + 1]);
+            }
+            // Check -Z option
+            else if (optionToFind == "-Z") {
+                if (argv[i + 1] == nullptr) {
+                    exit_ims("!!!You are missing argument for -Z parameter!!!");
+                }
+                else if(atoi(argv[i + 1]) > 1440){
+                    exit_ims("!!!-Z parameter can't be higher than 1440!!!");
+                }
+                string tmp = argv[i + 1];
+                if (atoi(argv[i + 1]) > 100  || tmp[0] == '-') {
+                    exit_ims("!!!-Z parameter must be between 0 and 100!!!");
+                }
+                args.Z = atoi(argv[i + 1])*60;
+            }
+            // Check -J option
+            else if (optionToFind == "-J") {
+                if (argv[i + 1] == nullptr) {
+                    exit_ims("!!!You are missing argument for -J parameter!!!");
+                }
+                string tmp = argv[i + 1];
+                if (atoi(argv[i + 1]) > 100  || tmp[0] == '-') {
+                    exit_ims("!!!-J parameter must be between 0 and 100!!!");
+                }
+                args.J = stoul(argv[i + 1], nullptr, 10);
+            }
+            // Check -K option
+            else if (optionToFind == "-K") {
+                if (argv[i + 1] == nullptr) {
+                    exit_ims("!!!You are missing argument for -K parameter!!!");
+                }
+                args.K = atoi(argv[i + 1]);
+            }
+            // Check -M option
+            else if (optionToFind == "-M") {
+                if (argv[i + 1] == nullptr) {
+                    exit_ims("!!!You are missing argument for -M parameter!!!");
+                }
+                string tmp = argv[i + 1];
+                if (atoi(argv[i + 1]) > 100  || tmp[0] == '-') {
+                    exit_ims("!!!-M parameter must be between 0 and 100!!!");
+                }
+                args.M = atoi(argv[i + 1]);
+            }
+            // Check -N option
+            else if (optionToFind == "-N") {
+                if (argv[i + 1] == nullptr) {
+                    exit_ims("!!!You are missing argument for -N parameter!!!");
+                }
+                string tmp = argv[i + 1];
+                if (tmp[0] == '-') {
+                    exit_ims("!!!-N parameter can't be negative!!!");
+                }
+                args.N = atoi(argv[i + 1]);
+            }
+        }
+    }
+    cout << "*** ENTERED ARGS ***" << endl; // TODO DELETE
+    cout << "R: " << args.R << " Qassams/day"<< endl; // TODO DELETE
+    cout << "Z: " << args.Z << " sec" << endl; // TODO DELETE
+    cout << "J: " << args.J << " %" << endl; // TODO DELETE
+    cout << "K: " << args.K << " %" << endl; // TODO DELETE
+    cout << "M: " << args.M << " %" << endl; // TODO DELETE
+    cout << "N: " << args.N << " sec"<< endl; // TODO DELETE
+    cout << "*********************" << endl; // TODO DELETE
+
+
+//    Print("***** MODEL1 *****\n");
+//    Init(0,1000);              // inicializace experimentu
+//    (new Generator)->Activate(); // generátor zákazníků, aktivace
+//    Run();                     // simulace
+//    Box.Output();              // tisk výsledků
+//    Tabulka.Output();
+        return 0;
+
+
+
+//    Print(" model2 - SIMLIB/C++ example\n");
+//    SetOutput("model2.out");
+//    Init(0,1000);              // experiment initialization for time 0..1000
+//    (new Generator)->Activate(); // customer generator
+//    Run();                     // simulation
+//    Box.Output();              // print of results
+//    Table.Output();
+//    return 0;
+    }
--- main.h
+++ main.h
@@ -10,33 +10,75 @@
 #include <iostream>
 #include <string>
 #include <simlib.h>
+#include <string.h>
 
 #define RED_BEGIN "\033[0;31m"
 #define CYAN_BEGIN "\033[1;36m"
 #define COLOR_END "\033[0m"
-#define BUFFER_SIZE 2048
 
 using namespace std;
+// seconds in day
+#define DAY 86400;
+
+// Struct for storing arguments
+// Argument have default value in case user doesn't enter them
+struct argValues{
+    // How many Qassams per day
+    unsigned int R = 300;
+    // How long it takes to fire all rockets per day [seconds]
+    unsigned int Z = 21600;
+    // Human error in BMC in %
+    unsigned int J = 1;
+    // Qassam failure rate in %
+    unsigned int K = 20;
+    // Qassams that need operator intervention in %
+    unsigned int M = 5;
+    // Uniform time of Qassam flight [seconds]
+    unsigned int N = 30;
+};
+
+//// global objects:
+//Facility  Box("Box");
+//Histogram Table("Table",0,25,20);
+//
+//class Customer : public Process {
+//    double Prichod;                 // attribute of each customer
+//    void Behavior() {               // --- behavior specification ---
+//        Prichod = Time;               // incoming time
+//        Seize(Box);                   // start of service
+//        Wait(10);                     // time of service
+//        Release(Box);                 // end of service
+//        Table(Time-Prichod);          // waiting and service time
+//    }
+//};
+//
+//class Generator : public Event {  // model of system's input
+//    void Behavior() {               // --- behavior specification ---
+//        (new Customer)->Activate();   // new customer
+//        Activate(Time+Exponential(1e3/150));  //
+//    }
+//};
 
 
-// global objects:
-Facility  Box("Box");
-Histogram Table("Table",0,25,20);
 
-class Customer : public Process {
-    double Prichod;                 // attribute of each customer
-    void Behavior() {               // --- behavior specification ---
-        Prichod = Time;               // incoming time
-        Seize(Box);                   // start of service
-        Wait(10);                     // time of service
-        Release(Box);                 // end of service
-        Table(Time-Prichod);          // waiting and service time
+//  deklarace  globálních  objektů
+Facility  Box("Linka");
+Histogram Tabulka("Tabulka",0,50,10);
+
+class Zakaznik : public Process { // třída zákazníků
+    double Prichod;                 // atribut každého zákazníka
+    void Behavior() {               // popis chování zákazníka
+        Prichod = Time;               // čas příchodu zákazníka
+        Seize(Box);                   // obsazení zařízení Box
+        Wait(10);                     // obsluha
+        Release(Box);                 // uvolnění
+        Tabulka(Time-Prichod);        // doba obsluhy a čekání
     }
 };
 
-class Generator : public Event {  // model of system's input
-    void Behavior() {               // --- behavior specification ---
-        (new Customer)->Activate();   // new customer
-        Activate(Time+Exponential(1e3/150));  //
+class Generator : public Event {  // generátor zákazníků
+    void Behavior() {               // popis chování generátoru
+        (new Zakaznik)->Activate();     // nový zákazník v čase Time
+        Activate(Time+Exponential(1e3/150)); // interval mezi příchody
     }
 };
